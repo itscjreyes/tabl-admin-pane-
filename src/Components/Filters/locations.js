99ebd35d9cc73a417777f862510ddb3082f5aa40
CJ Reyes <itscjreyes@gmail.com>
2020-06-05 08:51:25 -0400

    Location update

---
+++ src/Components/Filters/locations.js
@@ -0,0 +1,28 @@
+export const cityOptions = [
+    { value: 'toronto', label: 'Toronto' },
+    { value: 'nyc', label: 'NYC' }
+]
+
+export const stateOptions = [
+    { value: 'ontario', label: 'Ontario' }
+]
+
+export const countryOptions = [
+    { value: 'canada', label: 'Canada' },
+    { value: 'usa', label: 'USA' }
+]
+
+export const groupedOptions = [
+    {
+      label: 'City',
+      options: cityOptions,
+    },
+    {
+      label: 'State/Province',
+      options: stateOptions,
+    },
+    {
+      label: 'Country',
+      options: countryOptions,
+    },
+  ];
